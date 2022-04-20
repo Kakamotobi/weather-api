7d6ad5ce8b9ced994f6946e97697e9f508047693
Daeram Chung <daeram.chung@gmail.com>
2022-04-21 03:15:00 +0900

    Connect to APIs

--- src/js/helper.js
+++ src/js/helper.js
@@ -1,39 +1,181 @@
-// Function for Displaying Information
-function displayInfo(timezoneDisplay, timezoneData) {
-	const { timezone, weatherIcon, tempDegree, tempUnit, tempDesc } =
-		timezoneDisplay;
-	const { dataCity, dataCtry, dataIcon, dataTemp, dataDesc } = timezoneData;
-	timezone.textContent = `${dataCity}, ${dataCtry}`;
-	weatherIcon.innerHTML = `<img src="https:${dataIcon}" width="70px" height="70px" />`;
-	tempDegree.textContent = dataTemp;
-	tempUnit.textContent = "°C";
-	tempDesc.textContent = dataDesc;
-}
+// Find Background Image
+const findImg = (code) => {
+	if (code === 1000) return "clear";
+	else if (code === 1003 || 1006) return "cloudy";
+	else if (code === 1009) return "overcase";
+	else if (code === 1030 || code === 1135 || code === 1147) return "fog";
+	else if (
+		code === 1063 ||
+		code === 1150 ||
+		code === 1153 ||
+		code === 1180 ||
+		code === 1183 ||
+		code === 1240 ||
+		code === 1273
+	)
+		return "light-rain";
+	else if (
+		code === 1066 ||
+		code === 1210 ||
+		code === 1213 ||
+		code === 1255 ||
+		code === 1279
+	)
+		return "light-snow";
+	else if (
+		code === 1069 ||
+		code === 1072 ||
+		code === 1168 ||
+		code === 1171 ||
+		code === 1198 ||
+		code === 1201 ||
+		code === 1204 ||
+		code === 1207 ||
+		code === 1237 ||
+		code === 1249 ||
+		code === 1252 ||
+		code === 1261 ||
+		code === 1264
+	)
+		return "sleet";
+	else if (
+		code === 1186 ||
+		code === 1189 ||
+		code === 1192 ||
+		code === 1195 ||
+		code === 1243 ||
+		code === 1246 ||
+		code === 1276
+	)
+		return "heavy-rain";
+	else if (
+		code === 1114 ||
+		code === 1117 ||
+		code === 1216 ||
+		code === 1219 ||
+		code === 1222 ||
+		code === 1225 ||
+		code === 1258 ||
+		code === 1282
+	)
+		return "heavy-snow";
+	else if (code === 1087) return "thunder";
+};
 
-// Kelvin to Celsius Conversion
-function celsius(x) {
+// Format Time and Date Format
+const formatTimeAndDate = (dateStr) => {
+	const days = ["Sun", "Mon", "Tues", "Wed", "Thurs", "Fri", "Sat"];
+	const months = [
+		"Jan",
+		"Feb",
+		"Mar",
+		"Apr",
+		"May",
+		"Jun",
+		"Jul",
+		"Sep",
+		"Oct",
+		"Nov",
+		"Dec",
+	];
+
+	const today = new Date(dateStr);
+
+	const hours = today.getHours() % 12;
+	const mins = today.getMinutes().toString().padStart(2, "0");
+	const meridiem = today.getHours() >= 12 ? "PM" : "AM";
+	const day = days[today.getDay()];
+	const month = months[today.getMonth()];
+	const date = today.getDate();
+	const year = today.getFullYear();
+
+	return `${hours}:${mins}${meridiem} ${day}, ${month} ${date}, ${year}`;
+};
+
+// Rate Air Quality
+const rateAirQuality = (rate) => {
+	const ratings = [
+		null,
+		"Good",
+		"Moderate",
+		"Unhealthy (sensitive)",
+		"Unhealthy",
+		"Very Unhealthy",
+		"Hazardous",
+	];
+
+	// Set the rating text color
+	const re = new RegExp("[a-z]+", "g");
+	const className = ratings[rate].toLowerCase().match(re).join("-");
+	airQualityRatingDisplay.classList.remove(
+		...airQualityRatingDisplay.classList
+	);
+	airQualityRatingDisplay.classList.add(className);
+
+	return ratings[rate];
+};
+
+// Convert Kelvin to Celsius
+const kelvinToCelsius = (x) => {
 	return x - 273.15;
-}
+};
 
-// Kelvin to Fahrenheit Conversion
-function fahrenheit(x) {
-	return (x - 273.15) * (9 / 5) + 32;
-}
+// Fetch Data from API
+const fetchData = async (api1, api2) => {
+	const res1 = await fetch(api1);
+	const data1 = await res1.json();
+	const res2 = await fetch(api2);
+	const data2 = await res2.json();
+	return { ...data1, ...data2 };
+};
 
-// Function for Unit Conversion
-function unitConversion(timezoneDisplay, timezoneData) {
-	const { tempUnit, tempDegree, max, min } = timezoneDisplay;
-	const { dataTemp, dataMaxTemp, dataMinTemp } = timezoneData;
+// Display Data
+const displayData = (data) => {
+	const { location, current, name, sys, main } = data;
 
-	if (tempUnit.textContent === "°C") {
-		tempDegree.textContent = Math.round(fahrenheit(dataTemp));
-		tempUnit.textContent = "°F";
-		max.textContent = Math.round(fahrenheit(dataMaxTemp)) + "°";
-		min.textContent = Math.round(fahrenheit(dataMinTemp)) + "°";
+	// Background Image
+	const dayOrNight = current.is_day === 1 ? "day" : "night";
+	const imgTitle = findImg(current.condition.code);
+	body.style.backgroundImage = `url(src/images/weather/${dayOrNight}/${imgTitle}.png)`;
+	// Night Styles
+	if (dayOrNight === "night") {
+		body.classList.add("night");
 	} else {
-		tempDegree.textContent = Math.round(celsius(dataTemp));
-		tempUnit.textContent = "°C";
-		max.textContent = Math.round(celsius(dataMaxTemp)) + "°";
-		min.textContent = Math.round(celsius(dataMinTemp)) + "°";
+		body.classList.remove("night");
 	}
-}
+	// Main Panel
+	tempHighDisplay.textContent = Math.round(kelvinToCelsius(main.temp_max));
+	tempLowDisplay.textContent = Math.round(kelvinToCelsius(main.temp_min));
+	tempDisplay.innerHTML = `${Math.round(kelvinToCelsius(main.temp))}&#176;`;
+	locationDisplay.textContent = `${name}, ${sys.country}`;
+	timeDateDisplay.textContent = formatTimeAndDate(location.localtime);
+	weatherIconDisplay.innerHTML = `<img class="weather-icon-img" src="${current.condition.icon}" />`;
+	weatherDescDisplay.textContent = current.condition.text;
+	// Side Panel
+	feelsLikeDisplay.innerHTML = `${Math.round(
+		kelvinToCelsius(main.feels_like)
+	)}&#176;`;
+	uvIndexDisplay.textContent = current.uv;
+	windDisplay.textContent = `${current.wind_kph}km/h`;
+	humidityDisplay.textContent = `${current.humidity}%`;
+	precipitationDisplay.textContent = `${current.precip_mm}mm`;
+	visibilityDisplay.textContent = `${current.vis_km}km`;
+	airQualityRatingDisplay.textContent = rateAirQuality(
+		current.air_quality["us-epa-index"]
+	);
+	pm10Display.innerHTML = `${current.air_quality["pm10"].toFixed(
+		1
+	)}μg/m<sup>3</sup>`;
+	pm2_5Display.innerHTML = `${current.air_quality["pm2_5"].toFixed(
+		1
+	)}μg/m<sup>3</sup>`;
+	ozoneDisplay.innerHTML = `${current.air_quality["o3"].toFixed(
+		1
+	)}μg/m<sup>3</sup>`;
+	carbonMonoxideDisplay.innerHTML = `${current.air_quality["co"].toFixed(
+		1
+	)}μg/m<sup>3</sup>`;
+	nitrogenDioxideDisplay.innerHTML = `${current.air_quality["no2"].toFixed(
+		1
+	)}μg/m<sup>3</sup>`;
+};
--- src/js/index.js
+++ src/js/index.js
@@ -1,141 +1,29 @@
-const BASE_URL = "http://api.weatherapi.com/v1";
-const API_KEY = "004d4c8732b1442897341639222004";
-
-// ----------Current Timezone Weather---------- //
-
-// Select DOM Objects
-const currentTzTimezone = document.querySelector(".current-timezone__timezone");
-const currentTzWeatherIcon = document.querySelector(
-	".current-timezone__weatherIcon"
-);
-const currentTzMax = document.querySelector(".current-timezone__max");
-const currentTzMin = document.querySelector(".current-timezone__min");
-const currentTzTemp = document.querySelector(".current-timezone__temp");
-const currentTzTempDegree = document.querySelector(
-	".current-timezone__tempDegree"
-);
-const currentTzTempUnit = document.querySelector(".current-timezone__tempUnit");
-const currentTzTempDesc = document.querySelector(".current-timezone__tempDesc");
-
-// Current Timezone Display Object
-const currentTimezoneDisplay = {
-	timezone: currentTzTimezone,
-	weatherIcon: currentTzWeatherIcon,
-	max: currentTzMax,
-	min: currentTzMin,
-	temp: currentTzTemp,
-	tempDegree: currentTzTempDegree,
-	tempUnit: currentTzTempUnit,
-	tempDesc: currentTzTempDesc,
-};
-
 // Geolocation Event Listener
-window.addEventListener("load", function () {
-	// if browser supports geolocation
+window.addEventListener("load", () => {
+	// If browser supports geolocation
 	if (navigator.geolocation) {
-		navigator.geolocation.getCurrentPosition(
-			(position) => {
-				// Set Coordinates
-				const lon = position.coords.longitude;
-				const lat = position.coords.latitude;
+		navigator.geolocation.getCurrentPosition(async (position) => {
+			const lat = position.coords.latitude;
+			const lon = position.coords.longitude;
 
-				// API Current Timezone
-				const api = `${BASE_URL}/current.json?q=${lat},${lon}&key=${API_KEY}`;
-				fetch(api)
-					.then((res) => res.json())
-					.then((data) => {
-						const { location, current } = data;
-						// Current Timezone Data Object
-						const currentTimezoneData = {
-							dataCity: location.name,
-							dataCtry: location.country,
-							dataIcon: current.condition.icon,
-							dataTemp: current.temp_c,
-							dataDesc: current.condition.text,
-						};
+			const api1 = `${BASE_URL1}/current.json?q=${lat},${lon}&aqi=yes&key=${API_KEY1}`;
+			const api2 = `${BASE_URL2}/weather?lat=${lat}&lon=${lon}&appid=${API_KEY2}`;
 
-						// Update Display
-						displayInfo(currentTimezoneDisplay, currentTimezoneData);
-
-						// Convert between Celsius/Fahrenheit
-						currentTzTemp.addEventListener("click", () => {
-							unitConversion(currentTimezoneDisplay, currentTimezoneData);
-						});
-					})
-					// Error when something in the code doesn't work (ex: code, link trouble, etc.).
-					.catch((err) => {
-						console.log(err);
-					});
-			},
-			// Display error when user denies geolocation
-			(err) => (currentTzTempDesc.textContent = err.message)
-		);
-	}
-	// When browser doesn't support geolocation
-	else {
-		currentTzTempDesc.textContent = "Browser Doesn't Support Geolocation";
+			const data = await fetchData(api1, api2);
+			displayData(data);
+		});
+	} else {
+		timeDateDisplay.textContent = "Browser Doesn't Support Geolocation";
 	}
 });
 
-// ----------Other Timezone Weather---------- //
-
-// Select DOM Objects
-const form = document.querySelector("form");
-const otherTzTimezone = document.querySelector(".other-timezone__timezone");
-const otherTzWeatherIcon = document.querySelector(
-	".other-timezone__weatherIcon"
-);
-const otherTzMax = document.querySelector(".other-timezone__max");
-const otherTzMin = document.querySelector(".other-timezone__min");
-const otherTzTemp = document.querySelector(".other-timezone__temp");
-const otherTzTempDegree = document.querySelector(".other-timezone__tempDegree");
-const otherTzTempUnit = document.querySelector(".other-timezone__tempUnit");
-const otherTzTempDesc = document.querySelector(".other-timezone__tempDesc");
-
-// Other Timezone Display Object
-const otherTimezoneDisplay = {
-	timezone: otherTzTimezone,
-	weatherIcon: otherTzWeatherIcon,
-	temp: otherTzTemp,
-	tempDegree: otherTzTempDegree,
-	tempUnit: otherTzTempUnit,
-	tempDesc: otherTzTempDesc,
-};
-
-// Search Submit Event Listener
-form.addEventListener("submit", function (evt) {
+// Search Event Listener
+form.addEventListener("submit", async (evt) => {
 	evt.preventDefault();
-	const otherTzSearchCity = document.querySelector(
-		"#other-timezone__searchCity"
-	);
+	const api1 = `${BASE_URL1}/current.json?q=${searchInput.value}&aqi=yes&key=${API_KEY1}`;
+	const api2 = `${BASE_URL2}/weather?q=${searchInput.value}&appid=${API_KEY2}`;
 
-	// API Other Timezone
-	const api = `${BASE_URL}/current.json?q=${otherTzSearchCity.value}&key=${API_KEY}`;
-	fetch(api)
-		.then((res) => res.json())
-		.then((data) => {
-			const { location, current } = data;
-			// Other Timezone Data Object
-			const otherTimezoneData = {
-				dataCity: location.name,
-				dataCtry: location.country,
-				dataIcon: current.condition.icon,
-				dataTemp: current.temp_c,
-				dataDesc: current.condition.text,
-			};
-
-			// Update Display
-			displayInfo(otherTimezoneDisplay, otherTimezoneData);
-
-			// Convert between Celsius/Fahrenheit
-			otherTzTemp.addEventListener("click", () => {
-				unitConversion(otherTimezoneDisplay, otherTimezoneData);
-			});
-		})
-		.catch((err) => {
-			otherTzTimezone.textContent = "Try Again";
-		});
-
-	// Reset Search Value
-	otherTzSearchCity.value = "";
+	const data = await fetchData(api1, api2);
+	displayData(data);
+	searchInput.value = "";
 });
