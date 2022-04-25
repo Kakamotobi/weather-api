83edf1b73d6f5083b94bc479b4474ad58ce6c313
Daeram Chung <daeram.chung@gmail.com>
2022-04-25 18:29:08 +0900

    Add geolocation denied error

--- src/js/index.js
+++ src/js/index.js
@@ -2,16 +2,23 @@
 window.addEventListener("load", () => {
 	// If browser supports geolocation
 	if (navigator.geolocation) {
-		navigator.geolocation.getCurrentPosition(async (position) => {
-			const lat = position.coords.latitude;
-			const lon = position.coords.longitude;
+		navigator.geolocation.getCurrentPosition(
+			async (position) => {
+				const lat = position.coords.latitude;
+				const lon = position.coords.longitude;
 
-			const api1 = `${BASE_URL1}/current.json?q=${lat},${lon}&aqi=yes&key=${API_KEY1}`;
-			const api2 = `${BASE_URL2}/weather?lat=${lat}&lon=${lon}&appid=${API_KEY2}`;
+				const api1 = `${BASE_URL1}/current.json?q=${lat},${lon}&aqi=yes&key=${API_KEY1}`;
+				const api2 = `${BASE_URL2}/weather?lat=${lat}&lon=${lon}&appid=${API_KEY2}`;
 
-			const data = await fetchData(api1, api2);
-			if (data) displayData(data);
-		});
+				const data = await fetchData(api1, api2);
+				if (data) displayData(data);
+			},
+			(err) => {
+				locationDisplay.innerText = err.message;
+				timeDateDisplay.innerText =
+					"Try refreshing and allow the use of your location.";
+			}
+		);
 	} else {
 		locationDisplay.innerText = "Browser doesn't support geolocation";
 		timeDateDisplay.innerText = "Try searching";
